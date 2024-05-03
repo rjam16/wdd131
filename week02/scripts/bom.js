d1296517048294d491b933367c2d7197810b97fd
rjam16 <jam23019@byui.edu>
2024-05-02 19:24:59 -0700

    adjusting BOM app layout

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,9 +1,20 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('______');
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+const list = document.querySelector('#list');
+buttonElement.addEventListener('click', function() {
+    if (input.value.trim() !== '') 
+    {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+        list.append(li);
+    }
+});
+deleteButton.addEventListener('click', function () {
+    list.removeChild(li);
+    input.focus();
+});
+input.value = '';
+input.focus();
